f898e504fb9217862a95cb230c92ff3ddbd26dab
gram <git@orsinium.dev>
2024-04-21 14:46:55 +0200

    move FaQ into data

--- generate.py
+++ generate.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 import yaml
 from jinja2 import Environment, FileSystemLoader
+from jinja_markdown2 import MarkdownExtension
 
 
 env = Environment(loader=FileSystemLoader('templates'))
+env.add_extension(MarkdownExtension)
 public_dir = Path('public')
 public_dir.mkdir(exist_ok=True)
 template = env.get_template('index.html.j2')
