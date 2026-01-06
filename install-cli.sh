79bac20df11afe052e72f26d673d7902f84e5b76
gram <git@orsinium.dev>
2026-01-06 10:20:41 +0100

    get rid of bash functions

--- install-cli.sh
+++ install-cli.sh
@@ -1,81 +1,63 @@
 #!/bin/bash
 # Installation script for firefly_cli
-set -e
-set -o pipefail
 
-function get_latest_version() {
-  # TODO(@orsinium): curl might be not installed, fallback to wget.
-  local resp="$(curl https://api.github.com/repos/firefly-zero/firefly-cli/releases/latest)"
-  local tag="$(echo $resp | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
-  echo "${tag}"
-  return 0
-}
+set -o errexit   # abort on nonzero exit code
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
 
-function get_archive_suffix() {
-  local os=$(uname)
-  local arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
-  if [ "$os" = "Linux" ]; then
-    if [ "$arch" == "x86_64" ]; then
-      echo "-x86_64-unknown-linux-musl.tar.gz"
-      return 0
-    elif [ "$arch" == "i386" ] || [ "$arch" == "i686" ]; then
-      echo "-i686-unknown-linux-musl.tar.gz"
-      return 0
-    elif [[ "$arch" =~ ^arm ]]; then
-      echo "-arm-unknown-linux-musleabihf.tar.gz"
-      return 0
-    else
-      echo "Unsupported Linux architecture: $arch"
-      return 1
-    fi
-  elif [ "$os" = "Darwin" ]; then
-    if [ "$arch" == "aarch64" ]; then
-      echo "-aarch64-apple-darwin.tar.gz"
-      return 0
-    else
-      echo "Unsupported Apple architecture: $arch"
-      return 1
-    fi
+# Generate archive suffix based on the OS name and arch.
+os=$(uname)
+arch="$(uname -m | tr '[:upper:]' '[:lower:]')"
+if [ "$os" = "Linux" ]; then
+  if [ "$arch" == "x86_64" ]; then
+    suffix="-x86_64-unknown-linux-musl.tar.gz"
+  elif [ "$arch" == "i386" ] || [ "$arch" == "i686" ]; then
+    suffix="-i686-unknown-linux-musl.tar.gz"
+  elif [[ "$arch" =~ ^arm ]]; then
+    suffix="-arm-unknown-linux-musleabihf.tar.gz"
   else
-    # TODO(@orsinium): Windows with bash terminal.
-    # -i686-pc-windows-msvc.zip
-    # -x86_64-pc-windows-gnu.zip
-    # -x86_64-pc-windows-msvc.zip
-    echo "Unsupported OS"
-    return 1
+    echo "Unsupported Linux architecture: $arch"
+    exit 1
   fi
-}
+elif [ "$os" = "Darwin" ]; then
+  if [ "$arch" == "aarch64" ]; then
+    suffix="-aarch64-apple-darwin.tar.gz"
+  else
+    echo "Unsupported Apple architecture: $arch"
+    exit 1
+  fi
+else
+  # TODO(@orsinium): Windows with bash terminal.
+  # -i686-pc-windows-msvc.zip
+  # -x86_64-pc-windows-gnu.zip
+  # -x86_64-pc-windows-msvc.zip
+  echo "Unsupported OS"
+  exit 1
+fi
 
-function download_archive() {
-  local version="$(get_latest_version)"
-  local suffix="$(get_archive_suffix)"
-  local archive_name="firefly_cli-v${version}${suffix}"
-  local url="https://github.com/firefly-zero/firefly-cli/releases/download/${version}/${archive_name}"
-  local tmp_dir="$(mktemp -d)"
-  local file_path="${tmp_dir}/${archive_name}"
-  curl -L "${url}" -o "${file_path}"
-  echo "${file_path}"
-  return 0
-}
 
-function find_writable_path() {
-  return "."
-}
+# Get the latest release version.
+# TODO(@orsinium): curl might be not installed, fallback to wget.
+echo "Fetching latest release number..."
+resp="$(curl https://api.github.com/repos/firefly-zero/firefly-cli/releases/latest)"
+version="$(echo $resp | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')"
 
-function install_from_github() {
-  local archive_path="$(download_archive)"
-  local tmp_dir="$(dirname ${archive_path})"
-  tar -xzf "${archive_path}" -C "${tmp_dir}"
-  local out_dir="$(find_writable_path)"
-  mv "${tmp_dir}/*/firefly_cli" "${out_dir}/"
-}
+# Download archive
+echo "Downloading latest release..."
+archive_name="firefly_cli-v${version}${suffix}"
+echo "downloading ${archive_name}..."
+url="https://github.com/firefly-zero/firefly-cli/releases/download/${version}/${archive_name}"
+tmp_dir="$(mktemp -d)"
+archive_path="${tmp_dir}/${archive_name}"
+echo $url
+curl -L "${url}" -o "${archive_path}"
 
-function install() {
-  # if which cargo > /dev/null; then
-  #   cargo install firefly-cli
-  #   return
-  # fi
-  install_from_github
-}
+# Extract archive
+echo "Extracting archive..."
+tar -xzf "${archive_path}" -C "${tmp_dir}"
 
-install
+# TODO: find writable PATH.
+out_dir="."
+
+# Move binary to the selected path.
+mv "${tmp_dir}/*/firefly_cli" "${out_dir}/"
